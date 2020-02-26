5a6b8fa8c0255f9b3225e89b6b978eec408f65dc
startforever <1178507217@qq.com>
2020-02-26 08:25:55 +0800

    init

---
+++ HelloWorld.c
@@ -0,0 +1,7 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(int arg, char* argv[]){
+    printf("inited by SCM.\n");
+    printf("develop branch created by scm.\n");
+    printf("dev0102alter")
+}
